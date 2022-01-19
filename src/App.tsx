e6393aaf19352b23a833e8f4f62711786dc6e682
Sergey Scherbakov <falkr4m@yandex.ru>
2022-01-19 11:21:39 +0300

    Added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Fonts from './Components/Fonts/Fonts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fonts type='h1' text='Привет'/>
+      <Fonts type='h2' text='Привет'/>
+      <Fonts type='h3' text='Привет'/>
+      <Fonts type='h4' text='Привет'/>
+      <Fonts type='h5' text='Привет'/>
+      <Fonts type='h6' text='Привет'/>
+      <Fonts type='p' text='Привет'/>
     </div>
   );
 }
