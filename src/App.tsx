25a368b2e7f9c72b4946864a6152d3b4592f6b8c
Sergey Scherbakov <falkr4m@yandex.ru>
2022-01-19 14:16:33 +0300

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,26 @@ import headerLogo from './Components/Header/logo.svg';
 import Button from './Components/Button/Button';
 import Fonts from './Components/Fonts/Fonts';
 import Header from './Components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Header logo={headerLogo} logoText={'Tomato Genius'} />
-      <Fonts type="h1" secondary={true} text="Привет" />
-      <Fonts type="h2" text="Привет" />
-      <Fonts type="h3" text="Привет" />
-      <Fonts type="h4" text="Привет" />
-      <Fonts type="h5" text="Привет" />
-      <Fonts type="h6" text="Привет" />
-      <Fonts type="p" text="Привет" />
-      <Button label="Привет" hover={true} icon={logo} wide={false} />
+      <Header userAuth={false} logo={headerLogo} logoText={'Tomato Genius'} />
+      <div className="app">
+        <Button label="Привет" hover={true} icon={logo} wide={false} />
+        <Fonts type="h1" secondary={true} text="Привет" />
+        <Fonts type="h2" text="Привет" />
+        <Fonts type="h3" text="Привет" />
+        <Fonts type="h4" text="Привет" />
+        <Fonts type="h5" text="Привет" />
+        <Fonts type="h6" text="Привет" />
+        <Fonts type="p" text="Привет" />
+        <Routes>
+          <Route path="/signin" element={<Button label="какая то надпись" />} />
+          <Route path="/signup" element={<Button label="какая то надпись213" />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Header.css';
 import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
 
 interface HeaderProps {
+  userAuth: boolean;
   logo?: string;
   logoText?: string;
 }
@@ -21,8 +23,21 @@ class Header extends React.Component<HeaderProps> {
         </div>
         <nav className="header__nav">
           <Button label="О нас" />
-          <Button label="Профиль" />
-          <Button label="Настройки" />
+          {this.props.userAuth ? (
+            <>
+              <Button label="Профиль" />
+              <Button label="Настройки" />
+            </>
+          ) : (
+            <>
+              <Link to="/signin">
+                <Button label="Вход" />
+              </Link>
+              <Link to="/signup">
+                <Button label="Регистрация" />
+              </Link>
+            </>
+          )}
         </nav>
       </header>
     );
