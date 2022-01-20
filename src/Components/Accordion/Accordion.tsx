830d0d12959b547891856b4d2311470f01a0a7b1
Sergey Scherbakov <falkr4m@yandex.ru>
2022-01-20 13:01:18 +0300

    Added animation to accordion

--- src/Components/Accordion/Accordion.tsx
+++ src/Components/Accordion/Accordion.tsx
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './Accordion.css';
 import arrowSvg from './arrow.svg';
 import Fonts from '../Fonts/Fonts';
 
 interface AccordionProps {
   label?: string;
+  items?: Array<ReactNode>;
+  id?: string;
 }
 
 class Accordion extends React.Component<AccordionProps> {
+  accordion: React.RefObject<HTMLDivElement>;
+
+  constructor(props: AccordionProps) {
+    super(props);
+    this.accordion = React.createRef();
+  }
+
   toggle = (event: React.MouseEvent<HTMLButtonElement>) => {
     const { currentTarget } = event;
     const arrow = currentTarget.querySelector('.accordion-button__arrow');
     if (arrow) {
       arrow.classList.toggle('arrow-rotate');
     }
+
+    const list = this.accordion.current?.querySelector('.accordion__list');
+    list?.classList.toggle('accordion__list--opened');
   };
 
   render() {
     return (
-      <div className="accordion">
+      <div ref={this.accordion} className="accordion">
         <button onClick={this.toggle} className={'button accordion-button'}>
           {this.props.label}
           <img src={arrowSvg} className="accordion-button__arrow" alt="arrow img" />
         </button>
         <ul className="accordion__list">
+          {this.props.items?.map((item) => {
+            return (
+              <li key="2" className="accordion__list-item">
+                {item}
+              </li>
+            );
+          })}
           <li className="accordion__list-item">
             <Fonts type="h5" text="Название задачи" />
             <Fonts type="p" secondary={true} text={'Описание задачи'} />
--- src/Components/ToDoList/ToDoList.tsx
+++ src/Components/ToDoList/ToDoList.tsx
@@ -9,7 +9,7 @@ class ToDoList extends React.Component<unknown> {
       <div className="todo-list">
         <Fonts type="h2" text="Задачи" />
         <div className="todo-list__list">
-          <Accordion label="Задачи" />
+          <Accordion label="Задачи" items={[<Fonts key="1" type="h1" text="kek" />]} />
           <Accordion label="Сделано" />
         </div>
       </div>
