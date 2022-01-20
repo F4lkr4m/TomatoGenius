79b79b6f9f1ec44c533b8774fd57d8ff637c1412
Sergey Scherbakov <falkr4m@yandex.ru>
2022-01-20 21:30:26 +0300

    Added working functional to pomodoro timer

--- src/Components/PomodoroTimer/PomodoroTimer.tsx
+++ src/Components/PomodoroTimer/PomodoroTimer.tsx
@@ -5,32 +5,148 @@ import './PomodoroTimer.css';
 import Input from '../Input/Input';
 import stopSvg from '../../Assets/stop-button.svg';
 import playSvg from '../../Assets/play-button.svg';
+import pauseSvg from '../../Assets/pause-button.svg';
+import doneSvg from '../../Assets/done-button.svg';
+
+interface ButtonI {
+  onClick: () => void;
+  icon: string;
+}
 
 interface PomodoroState {
   mins: number;
   secs: number;
   timer: NodeJS.Timer | undefined;
+  active: boolean;
+  leftButton: ButtonI;
+  rightButton: ButtonI;
+  pomodoroNumber: number;
+  breakNumber: number;
+  pomodoroNumberCycle: number;
+  pomodoroQueue: Array<PomodoroTask>;
+  currentTask: PomodoroTask;
+}
+
+interface PomodoroTask {
+  task: 'pomodoro' | 'break';
+  mins: number;
+  secs: number;
 }
 
 class PomodoroTimer extends React.Component<unknown, PomodoroState> {
-  mins = 60;
-  secs = 0;
+  // Buttons aliases
+  playButton: ButtonI;
+  stopButton: ButtonI;
+  pauseButton: ButtonI;
+  doneButton: ButtonI;
 
   constructor() {
     super(null);
+
+    // Implementation of aliases
+    this.playButton = {
+      onClick: this.play,
+      icon: playSvg,
+    };
+    this.stopButton = {
+      onClick: this.stop,
+      icon: stopSvg,
+    };
+    this.doneButton = {
+      onClick: this.done,
+      icon: doneSvg,
+    };
+    this.pauseButton = {
+      onClick: this.pause,
+      icon: pauseSvg,
+    };
+
+    // Fix constants
+    const pomodoroQueue: Array<PomodoroTask> = [];
+    for (let i = 0; i < 4; i++) {
+      pomodoroQueue.push({
+        task: 'pomodoro',
+        mins: 60,
+        secs: 0,
+      });
+    }
+    pomodoroQueue.push({
+      task: 'break',
+      mins: 15,
+      secs: 0,
+    });
+
     this.state = {
       mins: 60,
       secs: 0,
+      active: false,
       timer: undefined,
+      leftButton: this.playButton,
+      rightButton: this.stopButton,
+      pomodoroNumber: 0,
+      pomodoroNumberCycle: 4,
+      breakNumber: 0,
+      pomodoroQueue: pomodoroQueue,
+      currentTask: pomodoroQueue[0],
     };
   }
 
-  play = () => {
-    console.log(this);
-    const timer = setInterval(() => {
-      this.countIteration();
-    }, 1000);
-    this.setState({ timer: timer });
+  private play = () => {
+    if (!this.state.active) {
+      const timer = setInterval(() => {
+        this.countIteration();
+      }, 1000);
+      this.setState({
+        timer: timer,
+        active: true,
+        leftButton: this.pauseButton,
+        rightButton: this.stopButton,
+      });
+    }
+  };
+
+  private pause = () => {
+    if (this.state.timer) {
+      clearInterval(this.state.timer);
+      this.setState({
+        timer: undefined,
+        active: false,
+        leftButton: this.playButton,
+        rightButton: this.doneButton,
+      });
+    }
+  };
+
+  private stop = () => {
+    if (this.state.timer && this.state.active) {
+      clearInterval(this.state.timer);
+      this.setState({
+        timer: undefined,
+        mins: 60,
+        secs: 0,
+        active: false,
+        leftButton: this.playButton,
+      });
+    }
+  };
+
+  private done = () => {
+    let newPomodoroNumber = this.state.pomodoroNumber;
+    let newBreakNumber = this.state.breakNumber;
+    if (this.state.currentTask.task === 'pomodoro') {
+      newPomodoroNumber += 1;
+    } else {
+      newBreakNumber += 1;
+    }
+    const nextStateIndex = (newPomodoroNumber + newBreakNumber) % this.state.pomodoroQueue.length;
+    const nextState = this.state.pomodoroQueue[nextStateIndex];
+    this.setState({
+      mins: nextState.mins,
+      secs: nextState.secs,
+      pomodoroNumber: newPomodoroNumber,
+      currentTask: nextState,
+      rightButton: this.stopButton,
+    });
   };
 
   private checkEndOfTimer() {
@@ -47,17 +163,11 @@ class PomodoroTimer extends React.Component<unknown, PomodoroState> {
       return;
     }
     if (this.checkEndOfTimer() && this.state.timer) {
-      clearInterval(this.state.timer);
-      this.setState({
-        timer: undefined,
-      });
+      this.stop();
     }
 
     if (this.state.secs === 1 && this.state.mins === 0) {
-      clearInterval(this.state.timer);
-      this.setState({
-        timer: undefined,
-      });
+      this.stop();
     }
 
     let newMins = this.state.mins;
@@ -73,44 +183,45 @@ class PomodoroTimer extends React.Component<unknown, PomodoroState> {
       mins: newMins,
       secs: newSecs,
     });
-
-    const minHtml = document.getElementById('min');
-    const secsHtml = document.getElementById('sec');
-
-    if (minHtml && secsHtml) {
-      minHtml.innerHTML = String(newMins);
-      secsHtml.innerHTML = String(newSecs);
-    }
-  };
-
-  stop = () => {
-    if (this.state.timer) {
-      clearInterval(this.state.timer);
-      this.setState({
-        timer: undefined,
-      });
-    }
   };
 
   render() {
+    let taskLabel = '';
+    if (this.state.currentTask.task === 'pomodoro') {
+      taskLabel = 'Помидор №' + this.state.pomodoroNumber;
+    } else {
+      taskLabel = 'Перерыв №' + this.state.breakNumber;
+    }
     return (
       <>
         <div className="pomodoro-timer">
           <div className="pomodoro-timer__time-box">
             <div id="min" className="pomodoro-timer__time">
-              60
+              {this.state.mins}
             </div>
             <div className="pomodoro-timer__time">:</div>
             <div id="sec" className="pomodoro-timer__time">
-              00
+              {this.state.secs}
             </div>
           </div>
           <menu className="pomodoro-timer__menu">
-            <Fonts type="h4" text="Помидор №1" />
+            <Fonts type="h4" text={taskLabel} />
             <Input type="text" placeholder="Цель помидора" />
             <div className="pomodoro-timer__control">
-              <Button onClick={this.play} wide={true} centered={true} icon={playSvg} type={'filled'} />
-              <Button onClick={this.stop} wide={true} centered={true} icon={stopSvg} type={'filled'} />
+              <Button
+                onClick={this.state.leftButton.onClick}
+                wide={true}
+                centered={true}
+                icon={this.state.leftButton.icon}
+                type={'filled'}
+              />
+              <Button
+                onClick={this.state.rightButton.onClick}
+                wide={true}
+                centered={true}
+                icon={this.state.rightButton.icon}
+                type={'filled'}
+              />
             </div>
           </menu>
         </div>
